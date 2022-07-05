c2e1b8ce0008983c23df85d8826360e34cda9c8d
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-05 01:07:30 -0300

    Update homework2.sql

--- modulo2/homework2.sql
+++ modulo2/homework2.sql
@@ -1,22 +1,37 @@
   --CONSULTAS
   
   --Países ordenados por nome decrescente:
-  SELECT * FROM VEMSER_PATRICIA.PAIS ORDER BY NOME DESC 
+  SELECT * 
+      FROM VEMSER_PATRICIA.PAIS 
+      ORDER BY NOME DESC 
   
   --Logradouro e cep dos endereços. Porém somente os logradouros que comecem com a letra a maiúsculo ou minúsculo
-  SELECT LOGRADOURO, CEP FROM VEMSER_PATRICIA.ENDERECO WHERE LOGRADOURO LIKE UPPER ('a%')
+  SELECT LOGRADOURO, CEP 
+      FROM VEMSER_PATRICIA.ENDERECO 
+      WHERE LOGRADOURO LIKE UPPER ('a%')
   
   --Enderecos que tenham cep com final 0
-  SELECT * FROM VEMSER_PATRICIA.ENDERECO WHERE CEP LIKE '%0'
+  SELECT * 
+      FROM VEMSER_PATRICIA.ENDERECO 
+      WHERE TRIM(CEP) LIKE  '%0'
 
   --Enderecos que tenham número entre 1 e 100
-  SELECT * FROM VEMSER_PATRICIA.ENDERECO WHERE NUMERO BETWEEN 1 AND 100
+  SELECT * 
+      FROM VEMSER_PATRICIA.ENDERECO 
+      WHERE NUMERO BETWEEN 1 AND 100
   
   --Enderecos que comecem por "rua" e ordenar cep de forma decrescente
-  SELECT * FROM VEMSER_PATRICIA.ENDERECO WHERE LOGRADOURO LIKE 'Rua%' ORDER BY CEP DESC
+  SELECT * 
+      FROM VEMSER_PATRICIA.ENDERECO 
+      WHERE LOGRADOURO LIKE UPPER ('Rua%') 
+      ORDER BY CEP DESC
   
   --Selecionar a quantidade de endereços cadastrados na tabela
-  SELECT COUNT (*) "Quantidade de endereços: " FROM VEMSER_PATRICIA.ENDERECO
+  SELECT COUNT (*) "Quantidade de endereços: " 
+      FROM VEMSER_PATRICIA.ENDERECO
   
   --Selecionar a quantidade de endereços agrupados pela id da cidade 
-  SELECT ID_CIDADE, COUNT (*) "Quantidade de endereços: " FROM VEMSER_PATRICIA.ENDERECO GROUP BY ID_CIDADE ORDER BY ID_CIDADE 
+  SELECT ID_CIDADE, COUNT (*) "Quantidade de endereços: " 
+      FROM VEMSER_PATRICIA.ENDERECO 
+      GROUP BY ID_CIDADE 
+      ORDER BY ID_CIDADE 
