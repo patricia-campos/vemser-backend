d2afe524c740c03242807a11c2891f37adc831fa
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-06 21:54:14 -0300

    Correção ContatoService

--- modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/service/ContatoService.java
+++ modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/service/ContatoService.java
@@ -1,38 +1,64 @@
 package br.com.vemser.pessoaapi.service;
 
 import br.com.vemser.pessoaapi.entity.Contato;
+import br.com.vemser.pessoaapi.entity.Pessoa;
+import br.com.vemser.pessoaapi.repository.PessoaRepository;
 import br.com.vemser.pessoaapi.repository.ContatoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 
 import java.util.List;
 
+@Service
 public class ContatoService {
 
+    @Autowired
     private ContatoRepository contatoRepository;
 
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
     public ContatoService(){
+        
         contatoRepository= new ContatoRepository();
     }
 
-    public Contato create(Contato contato){
+    public Contato create(Integer idPessoa, Contato contato) throws Exception{
+        Pessoa pessoa = pessoaRepository.list().stream()
+                .filter(x -> x.getIdPessoa().equals(idPessoa))
+                .findFirst().orElseThrow(() -> new Exception("Pessoa não localizada"));
+
+        contato.setIdPessoa(pessoa.getIdPessoa());
+
         return contatoRepository.create(contato);
     }
 
     public List<Contato> list(){
         return contatoRepository.list();
     }
+    
+    public Contato update(Integer id,Contato contato) throws Exception {
 
-    public Contato update(Integer id,
-                         Contato contatoAtualizar) throws Exception {
-        return contatoRepository.update(id, contatoAtualizar);
+        Contato contatoAlterado = contatoRepository.list().stream()
+                .filter(x -> x.getIdContato().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new Exception("Contato não econtrada"));
+
+        contatoAlterado.setTipo(contato.getTipo());
+        contatoAlterado.setNumero(contato.getNumero());
+        contatoAlterado.setDescricao(contato.getDescricao());
+
+        return contatoAlterado;
     }
 
     public void delete(Integer id) throws Exception {
+        
         contatoRepository.delete(id);
     }
 
     public List<Contato> listByIdPessoa(Integer idPessoa) {
+        
         return contatoRepository.listByIdPessoa(idPessoa);
     }
 }
-
-
