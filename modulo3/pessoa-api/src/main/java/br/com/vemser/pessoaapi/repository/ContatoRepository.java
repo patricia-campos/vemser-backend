8f1f4776421b6adc6a694b08a680bab6fe8d915c
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-06 21:57:27 -0300

    Correção ContatoRepository

--- modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/repository/ContatoRepository.java
+++ modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/repository/ContatoRepository.java
@@ -1,12 +1,14 @@
 package br.com.vemser.pessoaapi.repository;
 
 import br.com.vemser.pessoaapi.entity.Contato;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
+@Repository
 public class ContatoRepository {
 
     private static List<Contato> listaContatos = new ArrayList<>();
@@ -14,9 +16,9 @@ public class ContatoRepository {
 
     public ContatoRepository() {
         listaContatos.add(new Contato(COUNTER.incrementAndGet() /*1*/, 1, 2, "000333222", "Whatsapp"));
-        listaContatos.add(new Contato(COUNTER.incrementAndGet() /*2*/, 2, 1, "000333255", "Recados"));
+        listaContatos.add(new Contato(COUNTER.incrementAndGet() /*2*/, 2, 1, "000333255", "Código morse"));
         listaContatos.add(new Contato(COUNTER.incrementAndGet() /*3*/, 2, 2, "000333244", "Celular"));
-        listaContatos.add(new Contato(COUNTER.incrementAndGet() /*4*/, 3, 1, "000333233", "Whatsapp"));
+        listaContatos.add(new Contato(COUNTER.incrementAndGet() /*4*/, 3, 1, "000333233", "Sinal de fumaça"));
         listaContatos.add(new Contato(COUNTER.incrementAndGet() /*4*/, 3, 2, "000333277", "Telegram"));
     }
 
@@ -53,12 +55,10 @@ public class ContatoRepository {
                 .orElseThrow(() -> new Exception("Contato não localizado"));
         listaContatos.remove(contatoRecuperado);
     }
-
+    
     public List<Contato> listByIdPessoa(Integer idPessoa) {
         return listaContatos.stream()
-                .filter(contato -> contato.getIdPessoa().equals(idPessoa))
+                .filter(x -> x.getIdPessoa().equals(idPessoa))
                 .collect(Collectors.toList());
     }
 }
-
-
