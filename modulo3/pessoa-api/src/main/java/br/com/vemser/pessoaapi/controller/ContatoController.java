db8fa13ef154a0f0b324a73077d2b6a2fa39d649
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-06 21:56:02 -0300

    Correção ContatoController

--- modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/controller/ContatoController.java
+++ modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/controller/ContatoController.java
@@ -2,6 +2,7 @@ package br.com.vemser.pessoaapi.controller;
 
 import br.com.vemser.pessoaapi.entity.Contato;
 import br.com.vemser.pessoaapi.service.ContatoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -10,10 +11,9 @@ import java.util.List;
 
 public class ContatoController {
 
+    @Autowired
     private ContatoService contatoService;
-
-    public ContatoController() {contatoService = new ContatoService();}
-
+    
     // Listar todos os contatos - GET
     @GetMapping
     public List<Contato> list() {
@@ -21,15 +21,15 @@ public class ContatoController {
     }
 
     // Listar por pessoa - GET
-    @GetMapping("/byidpessoa")
-    public List<Contato> listByIdPessoa(@RequestParam("idPessoa") Integer idPessoa) {
+    @GetMapping("/{idPessoa}")
+    public List<Contato> listByIdCliente(@PathVariable("idPessoa") Integer idPessoa) {
         return contatoService.listByIdPessoa(idPessoa);
     }
 
     // POST com um id da pessoa para adicionar o contato e no corpo, receber o contato para inserir
-    @PostMapping
-    public Contato create(@RequestBody Contato contato) {
-        return contatoService.create(contato);
+    @PostMapping("/{idpessoa}")
+    public Contato create(@PathVariable("idPessoa") Integer id, @RequestBody Contato contato) throws Exception {
+        return contatoService.create(id, contato);
     }
 
     // PUT com o id do contato, deve atualizar um contato existente e receber no corpo da requisição
