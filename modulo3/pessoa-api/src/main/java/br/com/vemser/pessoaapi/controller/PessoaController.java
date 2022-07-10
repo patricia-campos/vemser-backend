adb97f54b8c02268a0d072b9fa23112ffee914bd
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-10 00:54:33 -0300

    -Pacote DTO criado -Removidas Entities de PessoaController -Aplicado ObjectMapper para converter em PessoaService

--- modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/controller/PessoaController.java
+++ modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/controller/PessoaController.java
@@ -1,5 +1,7 @@
 package br.com.vemser.pessoaapi.controller;
 
+import br.com.vemser.pessoaapi.dto.PessoaCreateDTO;
+import br.com.vemser.pessoaapi.dto.PessoaDTO;
 import br.com.vemser.pessoaapi.entity.Pessoa;
 import br.com.vemser.pessoaapi.service.PessoaService;
 
@@ -43,43 +45,53 @@ public class PessoaController {
     }
 
 
-
+    /*
     @PostMapping // Criar nova pessoa - localhost:8080/pessoa
     public ResponseEntity<Pessoa> create(@RequestBody @Valid  Pessoa pessoa) {
+        return ResponseEntity.ok(pessoaService.create(pessoa));
+    }
+     */
+
+
+    /*
+    @PutMapping("/{idPessoa}") // Editar pessoa existente - localhost:8080/pessoa/idPessoa
+    public ResponseEntity<Pessoa> update(@PathVariable("idPessoa") Integer id,
+                                         @Valid @RequestBody Pessoa pessoaAtualizar) throws Exception {
+
+        return ResponseEntity.ok(pessoaService.update(id, pessoaAtualizar));
+    }
+    */
+
+
+    //TODO - DTO POST E PUT EM CONSTRUÇÃO - INÍCIO -----------------------------------------------------
+    @PostMapping // Criar nova pessoa - localhost:8080/pessoa
+    public ResponseEntity<PessoaDTO> create(@RequestBody @Valid PessoaCreateDTO pessoa) {
         return ResponseEntity.ok(pessoaService.create(pessoa));
     }
 
 
     @PutMapping("/{idPessoa}") // Editar pessoa existente - localhost:8080/pessoa/idPessoa
-    public ResponseEntity<Pessoa> update(@PathVariable("idPessoa") Integer id,
-                         @Valid @RequestBody Pessoa pessoaAtualizar) throws Exception {
+    public ResponseEntity<PessoaDTO> update(@PathVariable("idPessoa") Integer id,
+                         @Valid @RequestBody PessoaCreateDTO pessoaAtualizar) throws Exception {
 
         return ResponseEntity.ok(pessoaService.update(id, pessoaAtualizar));
     }
+
+    //TODO - DTO POST E PUT EM CONSTRUÇÃO - FIM -----------------------------------------------------
 
 
     @DeleteMapping("/{idPessoa}") // Excluir pessoa - localhost:8080/pessoa/idPessoa
     public void delete(@PathVariable("idPessoa") Integer id) throws Exception {
         pessoaService.delete(id);
     }
-
-
-    /*
-    //TODO DÚVIDA: Precisa de Response Entity no delete? Pois com ResponseEntity ele exige um retorno.
-
-    // localhost:8080/pessoa/10
-    @DeleteMapping("/{idPessoa}")
-    public ResponseEntity<Pessoa> delete(@PathVariable("idPessoa") Integer id) throws Exception {
-
-        return ResponseEntity.ok(pessoaService.delete(id));
-    }
-
-     */
-
-    // localhost:8080/pessoa/hello
-    // @GetMapping("/hello")
-    //public String hello() {
-    //    return "Hello world!" + app;
-    //}
-
 }
+
+
+
+
+
+// localhost:8080/pessoa/hello
+// @GetMapping("/hello")
+//public String hello() {
+//    return "Hello world!" + app;
+//}
--- modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/service/PessoaService.java
+++ modulo3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/service/PessoaService.java
@@ -1,8 +1,11 @@
 package br.com.vemser.pessoaapi.service;
 
+import br.com.vemser.pessoaapi.dto.PessoaCreateDTO;
+import br.com.vemser.pessoaapi.dto.PessoaDTO;
 import br.com.vemser.pessoaapi.entity.Pessoa;
 import br.com.vemser.pessoaapi.exception.RegraDeNegocioException;
 import br.com.vemser.pessoaapi.repository.PessoaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +20,13 @@ public class PessoaService {
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    //TODO - DTO EM CONSTRUÇÃO ------------------------------------------------------
+    @Autowired  //TODO PESQUISAR PARA ENTENDER
+    private ObjectMapper objectMapper;
+    //TODO - DTO EM CONSTRUÇÃO ------------------------------------------------------
 
+
+    /*
     //----CREATE
     public Pessoa create(Pessoa pessoa) {
 
@@ -27,6 +36,25 @@ public class PessoaService {
         log.warn("Pessoa " + p.getNome() + " criada!");
         return pessoaRepository.create(p);
     }
+    */
+
+
+    //TODO - DTO CREATE EM CONSTRUÇÃO - INÍCIO -----------------------------------------------------
+    //----CREATE
+    public PessoaDTO create(PessoaCreateDTO pessoa) {
+
+        log.info("Criando a pessoa...");
+
+        Pessoa pessoaEntity = objectMapper.convertValue(pessoa, Pessoa.class);
+        Pessoa pessoaCriada = pessoaRepository.create(pessoaEntity);
+
+        PessoaDTO pessoaDTO = new PessoaDTO();
+        pessoaDTO = objectMapper.convertValue(pessoaCriada, PessoaDTO.class);
+
+        log.warn("Pessoa " + pessoaDTO.getNome() + " criada!");
+        return pessoaDTO;
+    }
+    //TODO - DTO CREATE EM CONSTRUÇÃO - FIM -----------------------------------------------------
 
 
     //----READ
@@ -39,6 +67,7 @@ public class PessoaService {
     }
 
 
+    /*
     //----UPDATE
     public Pessoa update(Integer id,
                          Pessoa pessoaAtualizar) throws Exception {
@@ -53,6 +82,33 @@ public class PessoaService {
         log.warn("Pessoa atualizada!");
         return pessoaRecuperada;
     }
+    */
+
+
+    //TODO - DTO UPDATE EM CONSTRUÇÃO - INÍCIO -----------------------------------------------------
+    //----UPDATE
+    public PessoaDTO update(Integer id,
+                            PessoaCreateDTO pessoaAtualizar) throws Exception {
+
+        Pessoa pessoaRecuperada = findPessoaById(id);
+
+        log.info("Atualizando pessoa " + pessoaRecuperada.getNome());
+
+        Pessoa pessoaEntity = objectMapper.convertValue(pessoaAtualizar, Pessoa.class);
+        pessoaRecuperada.setCpf(pessoaEntity.getCpf());
+        pessoaRecuperada.setNome(pessoaEntity.getNome());
+        pessoaRecuperada.setDataNascimento(pessoaEntity.getDataNascimento());
+
+
+        PessoaDTO pessoaDTO = new PessoaDTO();
+        pessoaDTO = objectMapper.convertValue(pessoaRecuperada, PessoaDTO.class);
+
+        log.warn("Pessoa atualizada!");
+
+
+        return pessoaDTO;
+    }
+    //TODO - DTO UPDATE EM CONSTRUÇÃO - FIM -----------------------------------------------------
 
 
     //----DELETE
@@ -79,18 +135,4 @@ public class PessoaService {
     }
 }
 
-    /*
-    //TODO DÚVIDA: Precisa de Response Entity no delete? Pois com ResponseEntity ele exige um retorno.
-    //----DELETE
-    public Pessoa delete(Integer id) throws Exception {
-
-        Pessoa pessoaRecuperada = findPessoaById(id);
-
-        log.info("Excluindo pessoa " + pessoaRecuperada.getNome());
-        pessoaRepository.list().remove(pessoaRecuperada);
-
-        log.warn("Pessoa excluída!");
-        return pessoaRecuperada;
-    }
-    */
 
