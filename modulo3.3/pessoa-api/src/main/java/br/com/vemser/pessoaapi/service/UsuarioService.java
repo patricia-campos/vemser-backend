9fecee421a624222ea96237e5424daf2f78795bc
Patrícia Campos <patricia.r.cam@gmail.com>
2022-07-28 07:36:38 -0300

    Add files via upload

--- modulo3.3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/service/UsuarioService.java
+++ modulo3.3/pessoa-api/src/main/java/br/com/vemser/pessoaapi/service/UsuarioService.java
@@ -1,8 +1,13 @@
 package br.com.vemser.pessoaapi.service;
 
+import br.com.vemser.pessoaapi.dto.UsuarioCreateDTO;
+import br.com.vemser.pessoaapi.dto.UsuarioDTO;
 import br.com.vemser.pessoaapi.entity.UsuarioEntity;
 import br.com.vemser.pessoaapi.repository.UsuarioRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,6 +16,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UsuarioService {
 
+    @Autowired
+    private ObjectMapper objectMapper;
     private final UsuarioRepository usuarioRepository;
 
     public Optional<UsuarioEntity> findByLoginAndSenha(String login, String senha) {
@@ -23,6 +30,18 @@ public class UsuarioService {
 
     public Optional<UsuarioEntity> findByLogin(String login) {
         return usuarioRepository.findByLogin(login);
+    }
+
+    public UsuarioDTO registrar(UsuarioCreateDTO usuarioCreateDTO){
+        UsuarioEntity usuarioEntity = objectMapper.convertValue(usuarioCreateDTO, UsuarioEntity.class);
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String senhaEncoder = encoder.encode(usuarioEntity.getSenha());
+
+        usuarioEntity.setSenha(senhaEncoder);
+        usuarioRepository.save(usuarioEntity);
+
+        return objectMapper.convertValue(usuarioEntity, UsuarioDTO.class);
     }
 
 }
